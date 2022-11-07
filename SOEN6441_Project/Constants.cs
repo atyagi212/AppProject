d28dbd26c5d3651d61cc097873297598c91b3d16
Devanshi1108 <42485563+Devanshi1108@users.noreply.github.com>
2022-11-07 16:55:31 -0500

    1) Added publishflight observer and details UI section. 2) Added contsants file for database support and javascript used in application.

---
+++ SOEN6441_Project/Constants.cs
@@ -0,0 +1,26 @@
+﻿namespace SOEN6441_Project
+{
+    public class Constants
+    {
+        public int currentFlightId;
+
+        public enum SubTables
+        {
+            DEPARTURE,
+            ARRIVAL,
+            AIRLINE,
+            FLIGHT,
+            CODESHARED
+        }
+
+        public void setCurrentFlightId(int value)
+        {
+            this.currentFlightId = value;
+        }
+
+        public int getCurrentFlightId()
+        {
+            return this.currentFlightId;
+        }
+    }
+}
---
+++ SOEN6441_Project/Interfaces/IDatabaseSubscriber.cs
@@ -0,0 +1,19 @@
+﻿using SOEN6441_Project.Entities.Output;
+
+namespace SOEN6441_Project.Interfaces
+{
+    public interface IDatabaseSubscriber
+    {
+        void update();
+
+        void CreateNewFlightData(ManifestResponseEntity response);
+
+        void ResetFlightsData();
+
+        int GetLatestFlightId();
+
+        void InsertComplexTables(FlightRecords tableType, DBContext context);
+
+        FlightRecords ParseFlightData(FlightRecords flightRecord);
+    }
+}
